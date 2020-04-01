12c4e499a742dd2cdb1535fcfff5b1aa791bccba
sergey <sergey.pyrkin@instatsport.com>
2020-04-02 00:01:27 +0300

    sizing

--- live/Program.cs
+++ live/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Threading;
 using live.Stages;
 
@@ -10,13 +11,32 @@ namespace live
         /*
          Точка входа.
          */
-       
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindowAsync(int hWnd, int nCmdShow);
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+
+        private static extern IntPtr GetConsoleWindow();
+        private static IntPtr ThisConsole = GetConsoleWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private const int HIDE = 0;
+        private const int MAXIMIZE = 3;
+        private const int MINIMIZE = 6;
+        private const int RESTORE = 9;
+
+
         public static void Main(string[] args)
         {
+            // Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            // ShowWindow(ThisConsole, MAXIMIZE);
+            ShowWindow(ThisConsole, MAXIMIZE);
+            Thread.Sleep(1000);
+            //Console.SetWindowSize(Console.WindowWidth, Console.LargestWindowHeight-1);
             var stage1 = new Stage1("ОБРАБОКА ФОТОГРАФИЙ");
-
-
-
 
             Console.WriteLine("");
             Console.WriteLine(DateTime.Now.ToString());
