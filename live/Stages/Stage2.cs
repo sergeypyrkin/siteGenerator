e28c8fa351c02b31d82533744d82fbd867be663f
sergey <sergey.pyrkin@instatsport.com>
2020-04-14 13:28:44 +0300

    get start index

--- live/Stages/Stage2.cs
+++ live/Stages/Stage2.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using live.Entity;
+using Microsoft.SqlServer.Server;
 
 namespace live.Stages
 {
@@ -13,7 +14,7 @@ namespace live.Stages
 
         //на этом этапе, копируем новый контент в папки data + приводим в порядок ихний info.txt
 
-
+        public int _stIndex = 1;
 
         public override void WORK()
         {
@@ -24,6 +25,32 @@ namespace live.Stages
                 return;
             }
 
+            foreach (var _ref in PATH.refs)
+            {
+                int stIndex = getStartIndex(_ref.destinationPass);
+
+            }
+
+        }
+
+        public int getStartIndex(string path)
+        {
+            int max = _stIndex;
+            string[] diA = Directory.GetDirectories(path);
+            List<int> names = new List<int>();
+            foreach (string s in diA)
+            {
+                String[] ll = s.Split(new string[] { "\\" }, StringSplitOptions.None);
+                String last = ll.Last();
+                int i = Convert.ToInt32(last);
+                names.Add(i);
+            }
+            if (names.Count > 0)
+            {
+                max = names.Max(t => t);
+                max = max + 1;
+            }
+            return max;
         }
 
 
