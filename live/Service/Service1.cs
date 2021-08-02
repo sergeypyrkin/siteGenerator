9ce9ad40049cd750a2c72ee3191aa6edc44c313e
sergey <sergey.pyrkin@instatsport.com>
2021-08-02 12:32:29 +0300

    parsing html file

--- live/Service/Service1.cs
+++ live/Service/Service1.cs
@@ -14,7 +14,7 @@ namespace live.Service
 
         public List<FileInfo> youtubs = new List<FileInfo>();
         public FileInfo html;
-
+        public List<string> ynames = new List<string>();
 
         public void EXECUTE()
         {
@@ -37,6 +37,36 @@ namespace live.Service
                         break;
                 }
             }
+            string content = FILEWORK.ReadFileContent(html.FullName);
+
+            foreach (var y in youtubs)
+            {
+                string name = y.Name.Replace(".mp4", "");
+                ynames.Add(name);
+            }
+
+            //тут собственно и парсинг
+            foreach (var yns in ynames)
+            {
+                 Console.WriteLine("===========");
+                 Console.WriteLine(yns);
+                 string reg = $">{yns}</a>";
+                 String[] row = content.Split(new string[] {reg}, StringSplitOptions.None);
+                 if (row.Length == 1)
+                 {
+                     Console.WriteLine("Error: неправильный парскинг, скорее всего есть виедо которго нету на канале (обычно такое происходит когда задвоится)" );
+                     Console.ReadKey();
+                     break;
+                 }
+                 String[] row2 = row[0].Split(new string[] {"href=" }, StringSplitOptions.None);
+                 string prelast = row2.Last();
+                 string last = prelast.Replace("\"", "");
+                 Console.WriteLine(last);
+            }
+
+
+
+            List<String> lines = content.Split(new char[] { '\t' }).ToList();
         }
     }
 }
